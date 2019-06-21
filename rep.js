b7302b987f8a2eeec82a54be4c0ee1a501cef56c
Ubuntu <ubuntu@ip-172-31-45-253.us-west-2.compute.internal>
2019-06-21 03:04:40 +0000

    initial NATS commit

---
+++ rep.js
@@ -0,0 +1,7 @@
+var servers = ['nats:iot-msg:4222'];
+var topic = 'trash-level';
+var nats = require('nats').connect({'servers':servers});
+
+nats.subscribe(topic, function(msg) {
+  console.log('Received report: ' + msg);
+});
